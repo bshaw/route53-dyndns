0f46b0df4228e82a4f800e0853277c4d53cfeb3a
Bradley Wood <git@bradleywood.com>
2019-02-03 13:21:53 +0000

    changed run script to refer to pluralised A record variable

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-  /usr/local/bin/r53dyndns.py -v -R $ROUTE53_DOMAIN_A_RECORD
+  /usr/local/bin/r53dyndns.py -v -R $ROUTE53_DOMAIN_A_RECORDS
   echo "Sleeping for $ROUTE53_UPDATE_FREQUENCY seconds..."
   sleep $ROUTE53_UPDATE_FREQUENCY
 done
